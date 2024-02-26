ec7f4ddf31400fecdd1d7bd0898115aae51fe8ce
hadis2000 <hadisehoseini6301@gmail.com>
2024-02-26 13:53:17 +0330

    fixed header and create tooltip

---
+++ app/compnent/common/tooltip/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+export type tooltipType = {
+  children: ReactNode;
+  text?: string;
+  item?: ReactNode;
+};
+
+const Tooltip = ({ children, text }: tooltipType) => {
+  return (
+    <div className="relative group">
+      {children}
+      <div className=" absolute top-[100%] left-[50%] -translate-x-[50%] hidden group-hover:block ">
+        <div className="w-2 h-2 rotate-45 bg-white absolute -top-1 left-[50%] -translate-x-[50%]"></div>
+        <div className="bg-white p-1 rounded-lg shadow-md">{text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
--- app/compnent/header/index.tsx
+++ app/compnent/header/index.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
 import HeaderItem from "./header-item";
+import Linkdin from "../../asset/img/2.png";
+import Instagram from "../../asset/img/3.png";
+import Telegram from "../../asset/img/5.png";
+import Tooltip from "../common/tooltip";
 
 const items = ["home", "about", "skills", "works", "contact us"];
 
 const Header = () => {
   return (
-    <header className="w-[90%] h-[70px] bg-white rounded-3xl mx-auto px-[2%] flex items-center justify-between mt-2">
-      <div className="border border-red-700">ddd</div>
+    <header
+      className="relative w-[90%] h-[80px] bg-white rounded-3xl mx-auto px-[2%] shadow-lg
+    flex md:flex-row flex-col-reverse items-center md:justify-between justify-center gap-2"
+    >
+      <div className="w-6 h-11 bg-white absolute -top-10 left-[10%]"></div>
+      <div className="w-6 h-11 bg-white absolute -top-10 right-[10%]"></div>
+      <div className="flex justify-between gap-3 *:cursor-pointer">
+        <Tooltip text="Linkdin">
+          <Image src={Linkdin} alt={"Linkdin"} width={30} height={30} />
+        </Tooltip>
+        <Tooltip text="Instagram">
+          <Image src={Instagram} alt={"Instagram"} width={30} height={30} />
+        </Tooltip>
+        <Tooltip text="Telegram">
+          <Image src={Telegram} alt={"Telegram"} width={30} height={30} />
+        </Tooltip>
+      </div>
       <div className="flex gap-7 h-fit">
         {items.map((itemName, index) => (
           <HeaderItem key={index} name={itemName} />
