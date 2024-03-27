c2f884e3273ce3f24b2dcc6c7c569abb31442508
hadis2000 <hadisehoseini6301@gmail.com>
2024-03-27 10:52:18 +0330

    add rotate animate to exp

--- app/compnent/exprience/component/all-exprience/index.tsx
+++ app/compnent/exprience/component/all-exprience/index.tsx
@@ -4,45 +4,47 @@ const AllExpriences = () => {
   const ex = [
     {
       name: "react",
-      className: "-top-5 left-[50%] -translate-x-[50%]",
+      className: "top-[0%] left-[43%]  ",
       color: "text-[#F6CBB3]",
     },
     {
       name: "next.js",
-      className: "top-[20%] -translate-y-[20%] left-12",
+      className: "top-[20%] left-[20%]",
       color: "text-[#C45798]",
     },
     {
       name: "typescript",
-      className: "top-[65%] -translate-y-[65%] left-4",
+      className: "top-[65%] left-[17%]",
       color: "text-[#AB1B1F]",
     },
     {
       name: "bootsrtrap",
-      className: "bottom-4 left-[50%] -translate-x-[50%]",
+      className: "top-[95%] left-[41%] ",
     },
     {
       name: "tailwindcss",
       color: "text-[#404F6F]",
-      className: "top-[65%] -translate-y-[65%] right-4",
+      className: "top-[65%]  right-[15%]",
     },
     {
       name: "javascript",
-      className: "top-[20%] -translate-y-[20%] right-12",
+      className: "top-[20%] right-[17%]",
       color: "text-[#DC677F]",
     },
   ];
   return (
-    <>
+    <div className="w-full h-full animate-spin-slow">
+      {/* <div className="w-full h-full "> */}
       {ex.map((it, index) => (
         <ExItem
           key={index}
           exName={it.name}
           textColor={it.color}
-          className={it.className}
+          className={it.className + " animate-spin-moon "}
+          // className={it.className + "  "}
         />
       ))}
-    </>
+    </div>
   );
 };
 
