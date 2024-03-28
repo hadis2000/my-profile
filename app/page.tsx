54d8a97a8c5e6cf0275079a227836a6ddbc29865
hadis2000 <hadisehoseini6301@gmail.com>
2024-03-28 22:32:36 +0330

    add section to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import Exprience from "./compnent/exprience";
 import FixedMenu from "./compnent/fixed-menu";
 import Header from "./compnent/header";
-import Introduction from "./compnent/introduction";
 import { useLanguage } from "./compnent/store/language";
-
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import HeroSection from "./section/hero-section";
 
 export default function Home() {
   const { lang } = useLanguage();
@@ -18,10 +16,7 @@ export default function Home() {
         className="min-h-screen bg-gradient-to-r from-defaultPink-50 to-defaultBlue-50 pt-8 relative"
       >
         <Header />
-        <div className="flex lg:flex-row flex-col justify-between gap-7 w-[90%] mx-auto overflow-hidden">
-          <Introduction />
-          <Exprience />
-        </div>
+        <HeroSection />
         <FixedMenu />
       </main>
     </I18nextProvider>
---
+++ app/section/hero-section/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Exprience from "../../compnent/exprience";
+import Introduction from "../../compnent/introduction";
+
+const HeroSection = () => {
+  return (
+    <div className="flex lg:flex-row flex-col justify-between gap-7 w-[90%] mx-auto overflow-hidden">
+      <Introduction />
+      <Exprience />
+    </div>
+  );
+};
+
+export default HeroSection;
