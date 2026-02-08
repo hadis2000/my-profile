5a35d9ec9ae9afd32a2780eeb95b4a6da77abcf4
hadis2000 <hadisehoseini6301@gmail.com>
2026-02-08 13:31:17 +0330

    install AOS

--- app/section/skills/item/index.tsx
+++ app/section/skills/item/index.tsx
@@ -5,14 +5,16 @@ const Item = () => {
     <div
       className="group w-32 h-32 mx-auto rounded-full shrink-0 overflow-hidden relative 
     flex items-center justify-center shadow"
+      data-aos="fade-in"
     >
       <CustomImage className="hover:blur-xs w-full h-full rounded-full" />
-      <p
+      <div
         className="absolute left-[50%] top-[50%] -translate-[50%] 
         text-[0px] group-hover:text-lg transition-all cursor-default"
       >
-        2 years
-      </p>
+        <p>REACT</p>
+        <p>2 years</p>
+      </div>
       {/* <div
         className="absolute inset-0 rounded-full bg-white/65 blur-md 
         translate-x-full -translate-y-10 group-hover:translate-x-0 group-hover:translate-y-0
