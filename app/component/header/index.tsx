d2abb6691098c6415de972fb6387b57e33cdd5ef
hadis2000 <hadisehoseini6301@gmail.com>
2026-02-19 10:44:26 +0330

    fix links

--- app/component/header/index.tsx
+++ app/component/header/index.tsx
@@ -1,10 +1,10 @@
 import HeaderItem from "./header-item";
 import Linkedin from "../../asset/img/2.png";
-import Instagram from "../../asset/img/3.png";
 import Telegram from "../../asset/img/5.png";
 import Tooltip from "../common/tooltip";
 import { useTranslation } from "react-i18next";
 import CustomImage from "../custom-image";
+import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
   const { t } = useTranslation();
@@ -22,22 +22,28 @@ const Header = () => {
       <div className="flex justify-between gap-3 *:cursor-pointer">
         <Tooltip text={t("linkedin")}>
           <CustomImage
+            onClick={() =>
+              window.open("https://linkedin.com/in/hadiseh-hosseini", "_blank")
+            }
             src={Linkedin}
             alt={t("linkedin")}
             width={30}
             height={30}
           />
         </Tooltip>
-        <Tooltip text={t("instagram")}>
-          <CustomImage
-            src={Instagram}
-            alt={t("instagram")}
-            width={30}
-            height={30}
+        <Tooltip text={t("github")}>
+          <FaGithub
+            className="text-3xl"
+            onClick={() =>
+              window.open("https://github.com/hadis2000", "_blank")
+            }
           />
         </Tooltip>
         <Tooltip text={t("telegram")}>
           <CustomImage
+            onClick={() =>
+              window.open("https://t.me/hadiseh_hosseini", "_blank")
+            }
             src={Telegram}
             alt={t("telegram")}
             width={30}
